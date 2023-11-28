eb54087b238d0ee864fd7281556e3b915fa223ba
Elizeu <el_vailant@hotmail.com>
2023-11-28 19:17:19 -0400

    model produto

--- BackEnd/models/produto.js
+++ BackEnd/models/produto.js
@@ -3,4 +3,109 @@ const bcrypt = require("bcryptjs");
 
 const Produto = {};
 
+Produto.getDadosProdutoPorId = (id, result) => {
+  const sql = `
+      select
+        *
+      from
+        produto AS P
+      WHERE 
+        P.id_produto = ?
+      ;
+    `;
+
+  db.query(sql, [id], (err, produto) => {
+    if (err) {
+      console.log("Erro:", err);
+      result(err, null);
+    } else {
+      console.log("Produto não encontrado:", produto);
+      result(null, produto);
+    }
+  });
+};
+
+Produto.setProduto = (produto, result) => {
+  const sql = `
+      INSERT INTO
+        produto(
+          nome,
+          vlr_produto,
+          descricao,
+          img
+        )
+      VALUES(?, ?, ?, ?)
+      ;
+    `;
+
+  db.query(
+    sql,
+    [produto.nome, produto.vlr_produto, produto.descricao, produto.img],
+    (err, produto) => {
+      if (err) {
+        console.log("Erro:", err);
+        result(err, null);
+      } else {
+        console.log("Id do novo produto:", produto.id_produto);
+        result(null, produto);
+      }
+    }
+  );
+};
+
+Usuario.updateProduto = (produto, result) => {
+  const sql = `
+    update 
+      produto
+    set
+      nome = ?,
+      vlr_produto = ?,
+      descricao = ?,
+      img = ?,
+    WHERE
+      id_produto = ?
+    ;
+  `;
+
+  db.query(
+    sql,
+    [
+      produto.nome,
+      produto.vlr_produto,
+      produto.descricao,
+      produto.img,
+      produto.id_produto,
+    ],
+    (err, produto) => {
+      if (err) {
+        console.log("Erro:", err);
+        result(err, null);
+      } else {
+        console.log("Produto atualizado:", produto);
+        result(null, produto);
+      }
+    }
+  );
+};
+
+Usuario.deleteProduto = (id_produto, result) => {
+  const sql = `
+    DELETE
+    FROM
+      produto
+    WHERE
+      id_produto = ?;
+    `;
+
+  db.query(sql, [id_produto], (err, res) => {
+    if (err) {
+      console.log("Erro:", err);
+      result(err, null);
+    } else {
+      console.log("Produto deletetado", id_produto);
+      result(null, id_produto);
+    }
+  });
+};
+
 module.exports = Produto;
