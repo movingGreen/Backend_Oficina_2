437bc4d1f3ec597242df25ffac91f083b5211b45
Elizeu <el_vailant@hotmail.com>
2023-11-28 23:17:53 -0400

    server pronto

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
-const logger = require("morgan");
 const cors = require("cors");
 const passport = require("passport");
 
@@ -10,8 +9,10 @@ const passport = require("passport");
  IMPORTAR ROTAS
 */
 const usuarioRotas = require("./routes/usuarioRotas");
-const produtosRotas = require("./routes/produtosRotas");
-const pedidosRotas = require("./routes/pedidosRotas");
+const produtoRotas = require("./routes/produtoRotas");
+const pedidoRotas = require("./routes/pedidoRotas");
+const pedido_produtoRotas = require("./routes/pedido_produtoRotas");
+const forma_pgtoRotas = require("./routes/forma_pgtoRotas");
 
 const port = process.env.PORT || 3000;
 
@@ -32,9 +33,11 @@ app.disable("x-powered-by");
 app.set("port", port);
 
 //  CHAMANDO AS ROTAS
-usuarioRotas(app, upload);
-produtosRotas(app);
-orderRoutes(app);
+usuarioRotas(app);
+produtoRotas(app);
+pedidoRotas(app);
+pedido_produtoRotas(app);
+forma_pgtoRotas(app);
 
 server.listen(3000, "192.168.0.4" || "localhost", function () {
   console.log("Aplicacao de NodeJS " + port + " Iniciada...");
