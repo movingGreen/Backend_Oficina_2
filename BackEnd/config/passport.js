010849942175bbbe75c4ccfe4c3314a480d330fe
Elizeu <el_vailant@hotmail.com>
2023-12-03 17:17:29 -0400

    desconsiderar

--- BackEnd/config/passport.js
+++ BackEnd/config/passport.js
@@ -1,30 +1,30 @@
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-const Keys = require('./keys');
-const User = require('../models/user');
+// const JwtStrategy = require('passport-jwt').Strategy;
+// const ExtractJwt = require('passport-jwt').ExtractJwt;
+// const Keys = require('./keys');
+// const User = require('../models/user');
 
-module.exports = (passport) => {
+// module.exports = (passport) => {
 
-    let opts = {};
-    opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
-    opts.secretOrKey = Keys.secretOrKey;
+//     let opts = {};
+//     opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
+//     opts.secretOrKey = Keys.secretOrKey;
 
-    passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
-    
-        User.findById(jwt_payload.id, (err, user) => {
+//     passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
 
-            if (err) {
-                return done(err, false);
-            }
-            if (user) {
-                return done(null, user);
-            }
-            else {
-                return done(null, false);
-            }
+//         User.findById(jwt_payload.id, (err, user) => {
 
-        });
+//             if (err) {
+//                 return done(err, false);
+//             }
+//             if (user) {
+//                 return done(null, user);
+//             }
+//             else {
+//                 return done(null, false);
+//             }
 
-    }));
+//         });
 
-}
+//     }));
+
+// }
